fddd8e74748a1b03cef0a1690cc15e843a2755a0
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-07-23 23:03:06 +0530

    Promises and asyncs 30 day js challenge

---
+++ Promises and Async/setInterval.js
@@ -0,0 +1,9 @@
+naam = "Renuka"
+const myFun = (naam = "Nitin")=>{
+    console.log(`Hello Narayana and ${naam}`);
+}
+
+const st = setInterval(myFun,1000)
+
+//clearInterval(st)
+
---
+++ Promises and Async/setTime.js
@@ -0,0 +1,8 @@
+naam = "Narayana"
+
+const myFun = function(){
+    console.log(`hey there i am Narayan here!!`);
+    }
+
+setTimeout(myFun,5000,naam)  // it can have third argument as well that we are passing into the function
+
